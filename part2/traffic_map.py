414cee1ec4313e8218d2a0cd9ff4d0cb180d9128
keichiroHiga <higa.devel@gmail.com>
2023-02-14 16:14:22 +0900

    fix: bug

--- part2/traffic_map.py
+++ part2/traffic_map.py
@@ -65,6 +65,6 @@ def show_map_data(G, routes, route_colors='r', ax=None) -> None:
 
     # 経路を可視化する
     if len(routes) == 1:
-        return ox.plot_graph_route(G, routes, bgcolor='#ffffff', node_color='#87cefa', route_color=route_colors, route_linewidth=2, route_alpha=0.8, orig_dest_size=100, ax=ax)
+        return ox.plot_graph_route(G, routes[0], bgcolor='#ffffff', node_color='#87cefa', route_color=route_colors, route_linewidth=2, route_alpha=0.8, orig_dest_size=100, ax=ax)
     else:
         return ox.plot_graph_routes(G, routes, bgcolor='#ffffff', node_color='#87cefa', route_colors=route_colors, route_linewidth=2, route_alpha=0.8, orig_dest_size=100, ax=ax)
