c75e1b6d0fb74788f465fef4d454a9fc47bde83f
keichiroHiga <higa.devel@gmail.com>
2023-05-17 11:11:02 +0900

    add: python310

--- colab.sh
+++ colab.sh
@@ -1,7 +1,2 @@
-pip install networkx
-pip install geopandas
-pip install osmnx
-pip install scikit-learn
-pip install xgboost
 pip install dwave-ocean-sdk
-pip install -U matplotlib==3.1.3
+pip install osmnx
